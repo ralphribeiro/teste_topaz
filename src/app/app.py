c17f5bbfef7f8f42c483d2ddc678d86a37fa3d40
ralphribeiro <ralph_ribeiro@msn.com>
2021-08-11 18:19:57 -0300

    primeiro commit

---
+++ src/app/app.py
@@ -0,0 +1,124 @@
+from dataclasses import dataclass, field
+from math import ceil
+
+
+@dataclass(eq=False)
+class Usuário:
+    ttask: int
+
+
+@dataclass
+class Servidor:
+    """ Objeto para processar ticks de tarefas de usuário. """
+    umax: int
+    custo_por_tick: int = 1
+    usuários: list[Usuário] = field(default_factory=list)
+    _total_ticks: int = 0
+
+    def __gt__(self, other) -> bool:
+        """ Pra comparar tempo de vida maior. """
+        return (
+            sum(u.ttask for u in self.usuários) - self._total_ticks >
+            sum(u.ttask for u in other.usuários) - other._total_ticks
+        )
+
+    def disponível(self) -> bool:
+        """ Disponível para novos usuários. """
+        return len(self.usuários) < self.umax
+
+    def adiciona_usuário(self, usuário: Usuário) -> None:
+        """ Adiciona usuário. """
+        if self.disponível():
+            self.usuários.append(usuário)
+
+    def _desconecta_finalizado(self) -> None:
+        """ Desconecta usuários com tarefas finalizadas. """
+        self.usuários = [u for u in self.usuários if u.ttask > 0]
+
+    def custo(self) -> int:
+        """ Calcula custo total por utilização. """
+        return self._total_ticks * self.custo_por_tick
+
+    def tick(self) -> None:
+        for u in self.usuários:
+            u.ttask -= 1
+        self._total_ticks += 1
+        self._desconecta_finalizado()
+
+    def a_finalizar(self) -> bool:
+        return not any(u.ttask for u in self.usuários)
+
+
+@dataclass
+class Balanceador:
+    """ Balanceador de cargas de tarefas de usuários em servidores. """
+    path_entrada: str
+    _ttask: int = field(init=False)
+    _umax: int = field(init=False)
+    _numero_novos_usuários: list = field(init=False)
+    _servidores: list[Servidor] = field(init=False, default_factory=list)
+    _custo_total: int = 0
+
+    def _le_entradas(self) -> None:
+        """ Obtem dados do arquido de entrada. """
+        with open(self.path_entrada) as f:
+            self._ttask = int(f.readline())
+            self._umax = int(f.readline())
+            self._numero_novos_usuários = [int(li) for li in f.readlines()]
+
+    def __post_init__(self):
+        self._le_entradas()
+
+    def _associa_usuários_servidores(self, n_usuários) -> None:
+        """ Cria usuários e os associam à servidores existentes ou criados. """
+        for _ in range(n_usuários):
+            usuário = Usuário(self._ttask)
+            s = next(
+                (s for s in sorted(self._servidores) if s.disponível()),
+                None
+            )
+            if not s:
+                s = Servidor(self._umax)
+                s.adiciona_usuário(usuário)
+                self._servidores.append(s)
+            else:
+                s.adiciona_usuário(usuário)
+
+    def _remove_servidor_ocioso(self) -> None:
+        """ remove o servidor a finalizar e acumula o custo. """
+        a_remover = [s for s in self._servidores if s.a_finalizar()]
+        for r in a_remover:
+            self._custo_total += r.custo()
+            self._servidores.remove(r)
+
+    def _tick(self, n_u) -> str:
+        """ Um manipulador para executar os ticks nos servidores e
+        retorna usuários ativos.
+        """
+        self._remove_servidor_ocioso()
+        self._associa_usuários_servidores(n_u)
+        usuarios_ativos = []
+        for s in self._servidores:
+            usuarios_ativos.append(len(s.usuários))
+            s.tick()
+        return ','.join(str(u) for u in usuarios_ativos)
+
+    def _obtém_numero_usuarios(self) -> int:
+        """ Obtém o número de usuários da fila. """
+        if len(self._numero_novos_usuários) > 0:
+            return self._numero_novos_usuários.pop(0)
+        else:
+            return 0
+
+    def processa_tarefas(self) -> str:
+        """ Loop principal de processamento das tarefas. """
+        saída = []
+        while True:
+            n_u = self._obtém_numero_usuarios()
+            saída.append(self._tick(n_u))
+            if len(self._servidores) == 0:
+                break
+
+        saída.append(str(self._custo_total))
+
+        return '\n'.join(s for s in saída if s)
---
+++ tests/test_unidade.py
@@ -0,0 +1,151 @@
+from os import path
+
+import pytest
+
+from src.app.app import Balanceador, Servidor, Usuário
+
+"""
+    tick - unidade básica de tempo de simulação
+
+    *<Usuários <conectam>> em <servidores <<disponíveis>> e executam uma
+<tarefa>.
+
+    *Cada <tarefa> leva um número de <ticks> para ser <finalizada> e após isso
+o <usuário se desconecta automaticamente>.
+
+    *O <número de ticks> é representado por <ttask>
+
+    Servidores são máquinas virtuais que se <auto criam> para acomodar novos
+usuários.
+
+    *Um servidor tem o <custo de R$1,00 por tick>.
+    *Um servidor <suporta no máximo <umax> usuários simultâneamente>.
+    Um servidor <deve ser finalizado se ocioso>.
+
+Entrada:
+    linha 1 - ttask
+    linha 2 - umax
+    demais  - número de usuários para cada tick
+
+
+Saída:
+    Um arquivo que contenha uma lista de servidores no final de cada tick,
+representado pelo número de usuários em cada servidor separados por vírgula
+e ao final, o custo total por utilização dos servidores.
+
+"""
+
+
+class TesteServidor:
+    def test_usuarios_conectam_em_servidores_disponíveis(self):
+        usuário = Usuário(ttask=1)
+        servidor = Servidor(umax=1)
+        servidor.adiciona_usuário(usuário)
+        assert usuário in servidor.usuários
+
+    def test_tarefa_6_ticks_decrementa_para_5_num_tick(self):
+        usuário = Usuário(ttask=6)
+        servidor = Servidor(umax=1)
+        servidor.adiciona_usuário(usuário)
+        servidor.tick()
+        assert usuário.ttask == 5
+
+    def test_usuario_se_desconecta_automaticamente(self):
+        usuário = Usuário(ttask=3)
+        usuário2 = Usuário(ttask=4)
+        servidor = Servidor(umax=2)
+        servidor.adiciona_usuário(usuário)
+        servidor.adiciona_usuário(usuário2)
+        for _ in range(3):
+            servidor.tick()
+        assert usuário not in servidor.usuários
+
+    def test_verifica_se_pode_ser_finalizado(self):
+        ticks = 2
+        usuário = Usuário(ttask=ticks)
+        servidor = Servidor(umax=2)
+        servidor.adiciona_usuário(usuário)
+        for _ in range(ticks):
+            servidor.tick()
+        assert servidor.a_finalizar()
+
+    def test_tarefa_5_ticks_a_1_real_custa_5_reais(self):
+        ttask = 5
+        usuário = Usuário(ttask=ttask)
+        servidor = Servidor(umax=1)
+        servidor.adiciona_usuário(usuário)
+        for _ in range(ttask):
+            servidor.tick()
+        assert servidor.custo() == 5
+
+    def test_permite_no_maximo_umax_2_usuários(self):
+        usuário1 = Usuário(ttask=2)
+        usuário2 = Usuário(ttask=2)
+        usuário3 = Usuário(ttask=2)
+        servidor = Servidor(umax=2)
+        servidor.adiciona_usuário(usuário1)
+        servidor.adiciona_usuário(usuário2)
+        servidor.adiciona_usuário(usuário3)
+        assert usuário1 in servidor.usuários
+        assert usuário2 in servidor.usuários
+        assert usuário3 not in servidor.usuários
+
+
+class TestBalanceador:
+    def test_carrega_entrada_válida(self):
+        balanceador = Balanceador(
+            path.join(path.dirname(__file__), 'input.txt')
+        )
+        assert balanceador._ttask == 4
+        assert balanceador._umax == 2
+        assert balanceador._numero_novos_usuários == [1, 3, 0, 1, 0, 1]
+
+    def test_na_comparação_o_maior_é_o_com_maior_ticks_a_processar(self):
+        balanceador = Balanceador(
+            path.join(path.dirname(__file__), 'input_test.txt')
+        )
+
+        usuário1 = Usuário(1)
+        usuário2 = Usuário(2)
+        usuário3 = Usuário(3)
+        usuário4 = Usuário(4)
+
+        servidor1 = Servidor(2)
+        servidor1.adiciona_usuário(usuário1)
+        servidor1.adiciona_usuário(usuário2)
+        servidor2 = Servidor(2)
+        servidor2.adiciona_usuário(usuário3)
+        servidor2.adiciona_usuário(usuário4)
+
+        assert servidor2 > servidor1
+
+    def test_cria_usuario_servidor(self):
+        balanceador = Balanceador(
+            path.join(path.dirname(__file__), 'input_test.txt')
+        )
+        balanceador._associa_usuários_servidores(1)
+        assert len(balanceador._servidores) == 1
+
+    def test_cria_usuários_em_servidores(self):
+        balanceador = Balanceador(
+            path.join(path.dirname(__file__), 'input_test2.txt')
+        )
+        balanceador._associa_usuários_servidores(2)
+        balanceador._associa_usuários_servidores(1)
+        assert len(balanceador._servidores[0].usuários) == 2
+        assert len(balanceador._servidores[1].usuários) == 1
+
+    def test_servidor_removido_quando_finalizado(self):
+        balanceador = Balanceador(
+            path.join(path.dirname(__file__), 'input_test.txt')
+        )
+        balanceador.processa_tarefas()
+        assert len(balanceador._servidores) == 0
+
+    def test_processa_tarefas(self):
+        balanceador = Balanceador(
+            path.join(path.dirname(__file__), 'input.txt')
+        )
+        esperado = '1\n2,2\n2,2\n2,2,1\n1,2,1\n2\n2\n1\n1\n15'
+        retorno = balanceador.processa_tarefas()
+        assert retorno == esperado
